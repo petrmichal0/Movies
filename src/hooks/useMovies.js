5abe3c0c748c88d2705a7e46fd73f6a30021d5d1
Petr Michal <petrmichal0@gmail.com>
2023-12-05 22:21:26 +0100

    update fetch url add http(s)

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -18,7 +18,7 @@ function useMovies(query) {
         setError("");
 
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+          `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
           { signal: controller.signal }
         );
 
